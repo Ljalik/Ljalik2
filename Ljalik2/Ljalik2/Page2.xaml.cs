3acc60e4473cc911393c021e2dc122ae957b90e9
opilane <opilane@HP-CZC6297B8C>
2020-11-09 17:02:46 +0200

    Ljalik2

---
+++ Ljalik2/Ljalik2/Page2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ljalik2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        Picker Picker;
+        public Page2()
+        {
+            Grid gr = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                },
+                    ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star)},
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star)},
+                }
+            };
+
+            Picker = new Picker
+            {
+                Title = "Keeled"
+            };
+            Picker.Items.Add("C#");
+            Picker.Items.Add("Python");
+            Picker.Items.Add("C++");
+            Picker.Items.Add("VisualBasic");
+            Picker.Items.Add("Java");
+
+            gr.Children.Add(Picker, 0, 0);
+
+            Content = gr;
+        }
+    }
+}
+           
